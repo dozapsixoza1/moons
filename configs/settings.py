c4214246a9429685b87d3633337631d1d9daf4d3
dozapsixoza1 <166742425+dozapsixoza1@users.noreply.github.com>
2025-09-11 02:06:58 +0400

    Create settings.py

---
+++ configs/settings.py
@@ -0,0 +1,7 @@
+token = '8325481151:AAF_ynMXDLoEBZQZaHHGb3k-V3FW0TTlSSc'
+dir = r'C:\\'
+version = '3.3'
+admins_id = [8185025058]
+
+standart_welcome = "Приветствуем вас в беседе!"
+tg_channel = 'https://t.me/moon_cm_bots'
